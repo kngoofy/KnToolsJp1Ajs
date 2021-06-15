bedc01e842e0e38c15a66e8418c16ff7cbe5dec3
Kazuhito Nishi <kngoofy@gmail.com>
2021-06-15 20:14:09 +0900

    Add Froms

---
+++ KnToolsJp1AjsForms/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace KnToolsJp1AjsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBoxAzNavel = new System.Windows.Forms.CheckBox();
+            this.checkBoxfBase = new System.Windows.Forms.CheckBox();
+            this.checkBoxDerico = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxAzNavel
+            // 
+            this.checkBoxAzNavel.AutoSize = true;
+            this.checkBoxAzNavel.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBoxAzNavel.Location = new System.Drawing.Point(70, 50);
+            this.checkBoxAzNavel.Name = "checkBoxAzNavel";
+            this.checkBoxAzNavel.Size = new System.Drawing.Size(116, 28);
+            this.checkBoxAzNavel.TabIndex = 0;
+            this.checkBoxAzNavel.Text = "AzNavel";
+            this.checkBoxAzNavel.UseVisualStyleBackColor = true;
+            this.checkBoxAzNavel.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBoxfBase
+            // 
+            this.checkBoxfBase.AutoSize = true;
+            this.checkBoxfBase.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBoxfBase.Location = new System.Drawing.Point(70, 91);
+            this.checkBoxfBase.Name = "checkBoxfBase";
+            this.checkBoxfBase.Size = new System.Drawing.Size(92, 28);
+            this.checkBoxfBase.TabIndex = 1;
+            this.checkBoxfBase.Text = "fBase";
+            this.checkBoxfBase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDerico
+            // 
+            this.checkBoxDerico.AutoSize = true;
+            this.checkBoxDerico.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBoxDerico.Location = new System.Drawing.Point(70, 138);
+            this.checkBoxDerico.Name = "checkBoxDerico";
+            this.checkBoxDerico.Size = new System.Drawing.Size(101, 28);
+            this.checkBoxDerico.TabIndex = 2;
+            this.checkBoxDerico.Text = "Derico";
+            this.checkBoxDerico.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(41, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1033, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 184);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(374, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 55);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(674, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 163);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 305);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBoxDerico);
+            this.Controls.Add(this.checkBoxfBase);
+            this.Controls.Add(this.checkBoxAzNavel);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxAzNavel;
+        private System.Windows.Forms.CheckBox checkBoxfBase;
+        private System.Windows.Forms.CheckBox checkBoxDerico;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ KnToolsJp1AjsForms/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KnToolsJp1Ajs;
+
+namespace KnToolsJp1AjsForms
+{
+    public partial class Form1 : Form
+    {
+        // Default System一覧 ブックを作成するSystemをチェック
+        Dictionary<string, bool> _dict = new Dictionary<string, bool>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            checkBoxAzNavel.Checked = _dict["AzNavel"] = true;
+            checkBoxfBase.Checked = _dict["fBase"] = true;
+            checkBoxDerico.Checked = _dict["Derico"] = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "jp1def (*.def)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+            //
+            var makebook = new AdapterMain();
+            var file = filePath.Replace(@"\\", @"\");
+            textBox1.Text = file;
+            makebook.MakeJp1DefBookAdapter(file);
+
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _dict["AzNavel"] = checkBoxAzNavel.Checked;
+            _dict["fBase"] = checkBoxfBase.Checked;
+            _dict["Derico"] = checkBoxDerico.Checked;
+        }
+
+        //JP1AJSのAJSPRINT出力形式ファイルをドロップしたときのブック作成処理実行
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            //
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            for (int i = 0; i < files.Length; i++)
+            {
+                string fileName = files[i];
+                textBox1.Text = fileName;
+            }
+
+            var makebook = new AdapterMain();
+            var file = textBox1.Text.Replace(@"\\", @"\");
+            textBox1.Text = file;
+            makebook.MakeJp1DefBookAdapter(file);
+
+        }
+
+        //JP1AJSのAJSPRINT出力形式ファイルをドロップしたときのチェック
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxAzNavel_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "jp1def (*.def)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+            //
+            var makebook = new AdapterMain();
+            var file = filePath.Replace(@"\\", @"\");
+            textBox1.Text = file;
+            makebook.MakeJp1DefBookAdapter(file);
+
+        }
+    }
+}
